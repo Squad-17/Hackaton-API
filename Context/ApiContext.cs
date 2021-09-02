19d71ce0724663828fe3749443a25fdee9e5ab9a
Matheus Carvalho <matheus10277@gmail.com>
2021-09-02 20:58:01 -0300

    Criando arquivo de context para entity framework

---
+++ Context/ApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Hackaton_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hackaton_API.Context
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+    }
+}
