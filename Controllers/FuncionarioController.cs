d62bdea6af7c04891b0b0dc2f4920a3edd3f4682
Matheus Carvalho <matheus10277@gmail.com>
2021-09-02 20:10:18 -0300

    Adicionando funcionarioController

---
+++ Controllers/FuncionarioController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hackaton_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuncionarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetFuncionarios()
+        {
+            return Ok(new string[] { "Matheus", "Renan", "Anderson" });
+        }
+    }
+}
