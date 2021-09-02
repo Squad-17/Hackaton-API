fb62755e368c71b3321d39236f039232bfcab517
Matheus Carvalho <matheus10277@gmail.com>
2021-09-02 20:58:57 -0300

    Adicionando apiContext ao configureServices

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,10 @@
+using Hackaton_API.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Hackaton_API
 {
@@ -26,6 +21,13 @@ namespace Hackaton_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ApiContext>(options =>
+            {
+                var conString = Configuration.GetConnectionString("MyCon");
+
+                options.UseMySql(conString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
