0d9b555161ff6523fdb472d66e01a14e871e8e9c
Matheus Carvalho <matheus10277@gmail.com>
2021-09-02 22:10:36 -0300

    Adicionando migration para email do funcionario

--- Migrations/20210903010638_Email-Funcionario.Designer.cs
+++ Migrations/20210903010638_Email-Funcionario.Designer.cs
@@ -2,14 +2,16 @@
 using Hackaton_API.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Hackaton_API.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    partial class ApiContextModelSnapshot : ModelSnapshot
+    [Migration("20210903010638_Email-Funcionario")]
+    partial class EmailFuncionario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
