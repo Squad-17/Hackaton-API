484fd4a9324e5dc458d46664f75febff2c2ab8de
Matheus Carvalho <matheus10277@gmail.com>
2021-09-03 20:57:40 -0300

    Implementando criação automática do banco de dados

--- Startup.cs
+++ Startup.cs
@@ -55,6 +55,12 @@ namespace Hackaton_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<ApiContext>();
+                context.Database.Migrate();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
