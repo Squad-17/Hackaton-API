6b2074cc9105d36b3ce82b719e89384bef5eb611
Matheus Carvalho <matheus10277@gmail.com>
2021-09-04 16:27:37 -0300

    Renomeando propriedades da classe Agendamento para o padrão do Entity Framework

--- Models/Agendamento.cs
+++ Models/Agendamento.cs
@@ -4,10 +4,10 @@ namespace Hackaton_API.Models
 {
     public class Agendamento
     {
-        public int IdLocal { get; set; }
+        public int LocalId { get; set; }
         public Local Local { get; set; }
         public DateTime Data { get; set; }
-        public int IdFuncionario { get; set; }
+        public int FuncionarioId { get; set; }
         public Funcionario Funcionario { get; set; }
     }
 }
