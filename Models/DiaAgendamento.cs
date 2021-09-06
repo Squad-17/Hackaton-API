65cfe96458b4cb88266f46fdf1f367176b5a075d
Matheus Carvalho <matheus10277@gmail.com>
2021-09-06 19:28:16 -0300

    Criando classe DiaAgendamento

---
+++ Models/DiaAgendamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hackaton_API.Models
+{
+    public class DiaAgendamento
+    {
+        public DateTime Data { get; set; }
+        public bool Disponivel { get; set; }
+
+        public DiaAgendamento(DateTime data)
+        {
+            Data = data;
+            Disponivel = true;
+        }
+    }
+}
