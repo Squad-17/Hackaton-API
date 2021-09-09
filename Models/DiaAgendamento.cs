1cedb9db9ea2cc9cd5837560f331b5791000ab9c
Matheus Carvalho <matheus10277@gmail.com>
2021-09-09 17:22:21 -0300

    Adicionando string do diaDaSemana na classe DiaAgendamento

--- Models/DiaAgendamento.cs
+++ Models/DiaAgendamento.cs
@@ -7,6 +7,16 @@ namespace Hackaton_API.Models
         public DateTime Data { get; set; }
         public bool Disponivel { get; set; }
 
+        public string DiaDaSemana
+        {
+            get
+            {
+                var diasDaSemana = new string[] { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+
+                return diasDaSemana[(int)Data.DayOfWeek];
+            }
+        }
+
         public DiaAgendamento(DateTime data)
         {
             Data = data;
